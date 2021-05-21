7008e913b12dc265b1ab8d9684b2b3221f1595aa
JasonYeon <jasonyeon110@gmail.com>
2021-05-20 22:06:43 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets play with flexbox</h1>
+      <h2>hello</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa eaque, vel saepe corporis adipisci dolor nostrum, tempora, illum fugiat amet facilis obcaecati eligendi. Voluptate, culpa quo numquam modi consectetur cumque.</p>
+    </div>
+  );
+}
+
+export default App;
