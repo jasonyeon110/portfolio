8809a53c8863dc6ae470319375de0a053cb15f9b
JasonYeon <jasonyeon110@gmail.com>
2021-05-20 22:22:14 -0700

    added React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import Stuffs from './Stuffs';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Lets play with flexbox</h1>
       <h2>hello</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa eaque, vel saepe corporis adipisci dolor nostrum, tempora, illum fugiat amet facilis obcaecati eligendi. Voluptate, culpa quo numquam modi consectetur cumque.</p>
+      <Stuffs />
     </div>
   );
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div>
+            <nav id="nav">
+                <a href="">
+                    Jason/Yeon
+                </a>
+                <ul>
+                    <li></li>
+                    <li>Profile</li>
+                    <li>Projects</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
