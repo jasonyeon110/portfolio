e81422937743a0eb3d77e85954a6dd267f841e44
JasonYeon <jasonyeon110@gmail.com>
2021-05-20 23:03:27 -0700

    fixed up Readme add links to index.html and edited css and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Stuffs from './Stuffs';
 function App() {
   return (
     <div className="App">
-      {/* <Nav /> */}
+      <Nav />
       <h1>Ji Woong Yeon's Portfolio</h1>
       <h2>hello I am a student at Northeastern University and currently studying Information Systems</h2>
       {/* <Stuffs /> */}
