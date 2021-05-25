3313dac33682526dd5761d622bea8d60332c20e7
JasonYeon <jasonyeon110@gmail.com>
2021-05-24 22:28:16 -0700

    editing Contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,20 +2,30 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <form id="contact-form" action="POST">
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control"/>
+        <div className="bg">
+            <div className="col-md-6">
+                <div className="well well-sm">
+
+                    <form id="contact-form" action="POST">
+                        <fieldset>
+                            <legend className="text-center">Contact</legend>
+                            <div className="form-group">
+                                <label htmlFor="name" className="col-md-3 control-label">Name</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="emailInput">Email Address</label>
+                                <input type="email" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="message">Message</label>
+                                <textarea id="msg" rows="7"></textarea>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </fieldset>
+                    </form>
+                </div>
             </div>
-            <div className="form-group">
-                <label htmlFor="emailInput">Email Address</label>
-                <input type="email" className="form-control" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="message">Message</label>
-                <textarea id="msg" rows="7"></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        </div>
     )
 }
